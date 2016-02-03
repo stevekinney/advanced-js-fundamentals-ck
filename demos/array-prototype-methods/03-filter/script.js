5f4c23a2b4ac91467d968c6029bacf8729070986
Steve Kinney <hello@stevekinney.net>
2016-02-03 11:26:47 -0700

    Add filter solutions

--- demos/array-prototype-methods/03-filter/script.js
+++ demos/array-prototype-methods/03-filter/script.js
@@ -1,15 +1,36 @@
 // TODO: Filter out just the photographs and store them in a variable called `photographs`.
 
-var photographs = instagramAPI.data;
+var photographs = instagramAPI.data.filter(function (asset) {
+  return asset.type === 'image';
+});
 
 // TODO: Filter out just the videos and store them in a variable called `videos`.
 
-var videos = instagramAPI.data;
+var videos = instagramAPI.data.filter(function (asset) {
+  return asset.type === 'video';
+});
 
 // TODO: Filter out all of the photographs that don't have any likes and store the remaining photographs into a variable called `popularPhotographs`.
 
-var popularPhotographs = photographs;
+var popularPhotographs = photographs.filter(function (photograph) {
+  return photograph.likes.count;
+});
 
 // TODO: Map each photograph into a DOM node and append it to the #photographs DIV element.
 
-// Your implementation here.
+var photographsDiv = document.getElementById('photographs');
+
+function createImageElementFromPhotograph(photograph) {
+  var image = document.createElement('img');
+  image.alt = photograph.caption.text;
+  image.src = photograph.images.low_resolution.url;
+  image.className = "instagram-image";
+  return image;
+}
+
+function addInstagramImageToThePage(photograph) {
+  var newImage = createImageElementFromPhotograph(photograph);
+  photographsDiv.appendChild(newImage);
+}
+
+photographs.forEach(addInstagramImageToThePage);
