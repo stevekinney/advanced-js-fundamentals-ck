f178bf96a3e97fe1b42b547018bda27c3d2a778f
Steve Kinney <hello@stevekinney.net>
2016-02-03 10:59:07 -0700

    Add map solution

--- demos/array-prototype-methods/02-map/script.js
+++ demos/array-prototype-methods/02-map/script.js
@@ -1,7 +1,12 @@
 // TODO: Create an array called `simplifiedPhotographs` that takes `photographs` and transforms each one into an object with only two properties: `caption` and `url`.
 
-var simplifiedPhotographs = photographs;
+var simplifiedPhotographs = photographs.map(function (photograph) {
+  return {
+    caption: photograph.caption.text,
+    url: photograph.images.low_resolution.url
+  }
+});
 
 // TODO: Create an array called `photographElements` that takes `simplifiedPhotographs` and transforms each one into a DOM element.
 
-var photographElements = simplifiedPhotographs;
+var photographElements = simplifiedPhotographs.map(createImageElementFromPhotograph);
